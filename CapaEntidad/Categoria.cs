3270bf7caaa129a9e600184123013ddace4bf890
milagrosna13 <milagrosnavarro1d@gmail.com>
2025-06-12 21:32:56 -0300

    actualizacion capa categoria

--- CapaEntidad/Categoria.cs
+++ CapaEntidad/Categoria.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//actualizacion de capa categoria
 namespace CapaEntidad
 {
     public class Categoria
